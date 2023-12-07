ccb88521fd7e24ce3319a455d8cb35b1a601197b
VoitovychDM <voitovych.dm.m@gmail.com>
2023-12-07 20:12:55 +0100

    fix added helpers and github action

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ export {
   IReduxLogout,
   IAuthResponse,
   IAuthUser,
-} from './auth.interface';
-export { IBuyerDocument, IReduxBuyer } from './buyer.interface';
+} from './interfaces/auth.interface';
+export { IBuyerDocument, IReduxBuyer } from './interfaces/buyer.interface';
 export {
   IConversationDocument,
   IMessageDocument,
@@ -23,7 +23,7 @@ export {
   IChatSellerProps,
   IChatBuyerProps,
   IChatMessageProps,
-} from './chat.interface';
+} from './interfaces/chat.interface';
 export {
   GigType,
   ICreateGig,
@@ -35,7 +35,7 @@ export {
   IGigViewReviewsProps,
   IGigInfo,
   IGigTopProps,
-} from './gig.interface';
+} from './interfaces/gig.interface';
 export {
   IOrderNotifcation,
   IOffer,
@@ -45,14 +45,14 @@ export {
   IOrderReview,
   IOrderMessage,
   IOrderDocument,
-} from './order.interface';
+} from './interfaces/order.interface';
 export {
   IReviewMessageDetails,
   IRatingTypes,
   IRatingCategories,
   IRatingCategoryItem,
   IReviewDocument,
-} from './review.interface';
+} from './interfaces/review.interface';
 export {
   ISearchResult,
   IHitsTotal,
@@ -60,7 +60,7 @@ export {
   IQueryString,
   ITerm,
   IPaginateProps,
-} from './search.interface';
+} from './interfaces/search.interface';
 export {
   SellerType,
   ILanguage,
@@ -68,8 +68,8 @@ export {
   IEducation,
   ICertificate,
   ISellerDocument,
-} from './seller.interface';
-export { IEmailLocals } from './email.interface';
+} from './interfaces/seller.interface';
+export { IEmailLocals } from './interfaces/email.interface';
 export { uploads, videoUpload } from './cloudinary-upload';
 export {
   IErrorResponse,
